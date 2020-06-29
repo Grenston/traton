554289f0d0f4f9e838900ab89cfcfce9ab3f5e6f
Grenston <grenston.george@ecanarys.com>
2020-06-29 17:44:07 +0530

    Update install_vs.ps1

--- install_vs.ps1
+++ install_vs.ps1
@@ -1,2 +1,3 @@
-Invoke-WebRequest "https://visualstudio.microsoft.com/thank-you-downloading-visual-studio/?sku=Community&rel=16" -OutFile vs_community.exe -UseBasicParsing
+Write-Host "Running script"
+#Invoke-WebRequest "https://visualstudio.microsoft.com/thank-you-downloading-visual-studio/?sku=Community&rel=16" -OutFile vs_community.exe -UseBasicParsing
 #.\vs_community.exe --installPath C:\VS2019 --add Microsoft.VisualStudio.Workload.CoreEditor --passive --norestart
